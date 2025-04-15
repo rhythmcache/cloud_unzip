12e7f2002554e580be2b04196e6f8b5c732ad05a
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-04-15 07:58:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="cloud_unzip",
-    version="0.1.0",
+    version="0.1.1",
     author="rhythmcache",
     description="python script to extract files from remote ZIP archives without downloading the entire archive",
     long_description=long_description,
